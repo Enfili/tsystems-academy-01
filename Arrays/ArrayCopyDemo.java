86f6f1e4e6cfa2340efe4095ee1429e0f205ec3f
Enfili <jaroslav.pavlicko@gmail.com>
2022-07-07 19:26:12 +0200

    homework 7/7/2022 v2.0

--- Arrays/ArrayCopyDemo.java
+++ Arrays/ArrayCopyDemo.java
@@ -4,6 +4,7 @@ public class ArrayCopyDemo {
 			    'i', 'n', 'a', 't', 'e', 'd' };
         char[] copyTo = new char[7];
 
+
         System.arraycopy(copyFrom, 2, copyTo, 0, 7);
         System.out.println(new String(copyTo));
 
