e07049a2e145faa216de7d8751632950d762e8d4
Jaroslav Pavlicko <jaroslav.pavlicko@gmail.com>
2022-07-07 13:51:23 +0200

    blablabla

--- src/HelloHelloWorld.java
+++ src/HelloHelloWorld.java
@@ -2,7 +2,7 @@ class HelloHelloWorld {
     public static void main(String[] jahoda) {
         int j = 0;
         do {
-            System.out.println("Hello World!");
+            System.out.println("Hello World! Hello World!");
             j++;
         } while (j < 2);
     }
